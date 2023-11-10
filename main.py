fee4a79f7db0f72a6cc6d111798ab131c15c8e78
Rudransh <aarti19830@gmail.com>
2023-11-10 16:31:36 +0530

    bug fixed

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import os
 from strings import *
 
 #loading 5 letter words from file
-words = ['hello','black','chick','sigma','apple','fruit','seven','voice','maths']
+# words = ['hello','black','chick','sigma','apple','fruit','seven','voice','maths']
+file_path = 'words.txt' 
+with open(file_path, 'r') as file:
+    words = [word.strip() for line in file for word in line.split() if len(word.strip()) == 5]
 
+# Now 'words_with_length_5' contains a list of words from the file with length exactly equal to 5
+# print(len(words))
+# print(words)
 
 #answer
 answer = random.choice(words)
@@ -20,7 +26,9 @@ num = 0
 won = False
 guessed = False
 
-while num!=5 and won==False and guessed==False:
+print("\Welcome to Wordle ! Predict the correct 5 letter word in 5 guesses  to WIN !! \n\-----Green : Correct letter in correct position \n\-----Yellow Correct Letter in Incorrect position  ")
+
+while num!=6 and won==False and guessed==False:
 
     input_word = input('Type the word :')
 
